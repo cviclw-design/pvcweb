ab8853b0e1bd87d4419072ccb7c47df8ae27186e
cviclw-design <cviclw@gmail.com>
2026-03-23 15:20:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -246,4 +246,5 @@ if __name__ == '__main__':
         init_db()
         # reload IEEMA once DB/app context is ready
         ieema_df = get_ieema_df()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
